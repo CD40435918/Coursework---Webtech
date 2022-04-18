46b2a98c3b20a7b6092a5316b331cbfff4c67ea5
CD40435918 <99051712+CD40435918@users.noreply.github.com>
2022-04-18 19:49:05 +0100

    Add files via upload

---
+++ quizbase.js
@@ -0,0 +1,7 @@
+function contactFunction(){
+	alert('CONTACT INFORMATION: 01234 56789      Email: quizbaseHQ@gmail.com');
+}
+
+function visitDashboard() {
+	window.location.href='dashboard.html';
+}
